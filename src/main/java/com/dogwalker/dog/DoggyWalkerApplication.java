980e7a11d8eb8111a6413664695fe69dbcac2087
akis220 <mavrommatis_t@yahoo.gr>
2020-07-04 19:13:29 +0300

    Initialized project with Spring Boot Initializer

---
+++ src/main/java/com/dogwalker/dog/DoggyWalkerApplication.java
@@ -0,0 +1,13 @@
+package com.dogwalker.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggyWalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggyWalkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogwalker/dog/DoggyWalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogwalker.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggyWalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
