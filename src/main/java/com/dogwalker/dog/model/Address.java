68e54d1f7d872503682245dea5977227c268f2f2
arglampedakis <glambedos18@hotmail.com>
2020-07-19 19:41:06 +0300

    Added Entity Classes

---
+++ src/main/java/com/dogwalker/dog/model/Address.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dogwalker.dog.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author glamb
+ */
+@Entity
+@Table(name = "ADDRESSES")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Address.findAll", query = "SELECT a FROM Addresses a"),
+    @NamedQuery(name = "Address.findByAddressId", query = "SELECT a FROM Address a WHERE a.addressId = :addressId"),
+    @NamedQuery(name = "Address.findByLongitude", query = "SELECT a FROM Address a WHERE a.longitude = :longitude"),
+    @NamedQuery(name = "Address.findByLatitude", query = "SELECT a FROM Address a WHERE a.latitude = :latitude"),
+    @NamedQuery(name = "Address.findByAddressType", query = "SELECT a FROM Address a WHERE a.addressType = :addressType"),
+    @NamedQuery(name = "Address.findByAddressDescription", query = "SELECT a FROM Address a WHERE a.addressDescription = :addressDescription")})
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ADDRESS_ID")
+    private Integer addressId;
+    @Basic(optional = false)
+    @Column(name = "LONGITUDE")
+    private int longitude;
+    @Basic(optional = false)
+    @Column(name = "LATITUDE")
+    private int latitude;
+    @Basic(optional = false)
+    @Column(name = "ADDRESS_TYPE")
+    private String addressType;
+    @Basic(optional = false)
+    @Column(name = "ADDRESS_DESCRIPTION")
+    private String addressDescription;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "addressId")
+    private Collection<AddressPhoto> addressPhotosCollection;
+    @JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID")
+    @ManyToOne(optional = false)
+    private User userId;
+
+    public Address() {
+    }
+
+    public Address(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public Address(Integer addressId, int longitude, int latitude, String addressType, String addressDescription) {
+        this.addressId = addressId;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.addressType = addressType;
+        this.addressDescription = addressDescription;
+    }
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(String addressType) {
+        this.addressType = addressType;
+    }
+
+    public String getAddressDescription() {
+        return addressDescription;
+    }
+
+    public void setAddressDescription(String addressDescription) {
+        this.addressDescription = addressDescription;
+    }
+
+    @XmlTransient
+    public Collection<AddressPhoto> getAddressPhotosCollection() {
+        return addressPhotosCollection;
+    }
+
+    public void setAddressPhotosCollection(Collection<AddressPhoto> addressPhotosCollection) {
+        this.addressPhotosCollection = addressPhotosCollection;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (addressId != null ? addressId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Address)) {
+            return false;
+        }
+        Address other = (Address) object;
+        if ((this.addressId == null && other.addressId != null) || (this.addressId != null && !this.addressId.equals(other.addressId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.dogwalker.dog.model.Address[ addressId=" + addressId + " ]";
+    }
+    
+}
