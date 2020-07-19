b7589b273ffbec9e0e6aef4a86f1c6a8606b93db
arglampedakis <glambedos18@hotmail.com>
2020-07-19 22:35:15 +0300

    Added Authentication Controller. Add methods to MyWebSecurityConfigurer (not working yet)

--- src/main/java/com/example/DoggyWalker/MyWebSecurityConfigurer.java
+++ src/main/java/com/example/DoggyWalker/MyWebSecurityConfigurer.java
@@ -5,9 +5,16 @@
  */
 package com.example.DoggyWalker;
 
+import com.example.DoggyWalker.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
@@ -16,10 +23,48 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class MyWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
+     @Autowired
+    private UserServiceImpl userServiceImpl;
+     
+     
+     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(authenticationProvider());
+    }
+     
+     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()//restrict access based on the HttpServletRequest
-                .antMatchers("/").permitAll();
+                .antMatchers("/").permitAll()
+                .antMatchers("/index").permitAll()
+                .and()
+                .formLogin() // We are customizing the form login process
+                .loginPage("/loginPage") // This is the url to show the login page
+                .permitAll() //Allow everyone to see login page. Don't have to be logged in.
+                .and()
+                .logout()
+                .and()
+                .exceptionHandling() // ti na kanw an exw exception?
+                .accessDeniedPage("/access-denied"); //phgaine edw
+    }
+    
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+
+//        auth.setUserDetailsService(userServiceImpl);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        return encoder;
     }
 }
---
+++ src/main/java/com/example/DoggyWalker/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.DoggyWalker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author glamb
+ */
+@Controller
+public class AuthenticationController {
+    
+    @GetMapping("/loginPage")
+    public String showLoginPage() {
+        return "login-form";
+    }
+    
+    @GetMapping("/access-denied")
+    public String showAccessDeniedPage() {
+        return "access-denied";
+    }
+}
